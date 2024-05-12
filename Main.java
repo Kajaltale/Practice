cdd7d3f63ae299807d7d1c18dd18b0691e4daaa7
Kajaltale <124420030+Kajaltale@users.noreply.github.com>
2024-05-12 20:50:49 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+ class Student {
+  String name;
+   int roll_no;
+   int age ;
+
+
+ }
+public class Main {
+
+    public static void main(String[] args) {
+	  //  Student s1 = new Student();
+
+
+    }
+}
